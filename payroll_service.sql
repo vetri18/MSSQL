8ae6db3c281686553172406aae546d4ee0053b8c
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-08 12:24:36 +0530

    complete pay roll service sql

--- payroll_service.sql
+++ payroll_service.sql
@@ -12,27 +12,27 @@ startDate Date
 );
 
 /* UC3 */
-Insert into employee_payroll values('vidhya',70000,GETDATE());
-Insert into employee_payroll values('Ganesh',80000,GETDATE());
-Insert into employee_payroll values('Vishal',60000,GETDATE());
-Insert into employee_payroll values('Vaibhav',180000,GETDATE());
-Insert into employee_payroll values('Vinayak',100000,'2018-01-01');
+Insert into employee_payroll values('kala',70000,GETDATE());
+Insert into employee_payroll values('Gagan',80000,GETDATE());
+Insert into employee_payroll values('Vishnu',60000,GETDATE());
+Insert into employee_payroll values('Vaishak',180000,GETDATE());
+Insert into employee_payroll values('Vinoth',100000,'2018-01-01');
 
 /* UC4 */
 Select * from employee_payroll;
 
 /* UC5 */
-SELECT salary FROM employee_payroll WHERE name = 'Vishal';
+SELECT salary FROM employee_payroll WHERE name = 'Vinoth';
 
 SELECT salary FROM employee_payroll WHERE startDate BETWEEN CAST('2018-01-01' AS DATE) AND GETDATE();
 
 /* UC6 */
 ALTER table employee_payroll Add  Gender char ;
-UPDATE employee_payroll SET Gender = 'F' where name = 'Vidhya';
-UPDATE employee_payroll SET Gender = 'M' where name = 'Ganesh';
-UPDATE employee_payroll SET Gender = 'M' where name = 'Vishal';
-UPDATE employee_payroll SET Gender = 'M' where name = 'Vaibhav';
-UPDATE employee_payroll SET Gender = 'M' where name = 'Vinayak';
+UPDATE employee_payroll SET Gender = 'F' where name = 'kala';
+UPDATE employee_payroll SET Gender = 'M' where name = 'Gagan';
+UPDATE employee_payroll SET Gender = 'M' where name = 'Vishnu';
+UPDATE employee_payroll SET Gender = 'M' where name = 'Vaishak';
+UPDATE employee_payroll SET Gender = 'M' where name = 'Vinoth';
 Select * from employee_payroll;
 
 /* UC7 */
